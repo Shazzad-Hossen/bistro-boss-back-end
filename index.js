6c415dd159a496afa526c29892d09b0ea1075c07
Shazzad Hossen <tipsfire.com@gmail.com>
2023-06-04 23:56:09 +0600

    some apies created

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require('dotenv').config()
 const express= require('express');
 const cors= require('cors');
 const port= process.env.PORT || 5000;
+const jwt= require('jsonwebtoken');
 const app= express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abirkqb.mongodb.net/?retryWrites=true&w=majority`;
 
 //middleware
@@ -23,8 +24,57 @@ async function run() {
     await client.connect();
     const database = client.db("bistroBoss");
     const menu = database.collection("menu");
+    const cart = database.collection("cart");
+    const users = database.collection("users");
+
+    app.post('/jwt',(req,res)=>{
+      const user= req.body;
+      const token= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'});
+        res.send(token);
+    })
+
+    //User related api
+    app.get('/users', async(req,res)=>{
+      const result= await users.find().toArray();
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id',async(req,res)=>{
+
+      const id= req.params.id;
+      const uRole= req.body.role;
+      const query= { _id: new ObjectId(id)};
+      const updatedDoc= {
+        $set: {
+          role: uRole
+        }
+
+      }
+      const result= await users.updateOne(query,updatedDoc);
+      res.send(result);
+
+    })
 
 
+
+
+    app.post('/users',async(req,res)=>{
+
+      const user=req.body;
+      const query= {email: user.email};
+      const isExist= await users.findOne(query);
+      
+      if(isExist) {
+        return res.send({message: 'user already exist'});
+
+      }
+      const result= await users.insertOne(user);
+      res.send(result);
+
+    })
+
+  //Menu related apies
     app.get('/menu/:cat',async(req,res)=>{
         const cat= req.params.cat;
         if(cat==='all') {
@@ -35,11 +85,43 @@ async function run() {
             const query = { category: cat };
         const cursor = await menu.find(query).toArray();
         res.send(cursor)
-        }
-        
-       
-        
-    })
+        }  
+    });
+
+
+
+
+
+   //Cart collection
+
+   app.post('/carts',async(req,res)=>{
+
+    const item = req.body;
+    const result= await cart.insertOne(item);
+    res.send(result);
+   })
+
+
+   app.get('/carts',async(req,res)=>{
+    const email= req.query.email;
+    if(!email) res.send([]);
+    const query= {email: email};
+    const cursor= await cart.find(query).toArray();
+    res.send(cursor)
+
+   });
+
+
+   app.delete('/carts/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query= { _id: new ObjectId(id)};
+    const result= await cart.deleteOne(query);
+    res.send(result) ;
+   });
+
+
+
+
 
 
  
